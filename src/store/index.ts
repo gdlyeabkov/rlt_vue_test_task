67924b9196c27e9862ba84081c9eecbe771ad205
internetlead <glebdyakov2000@gmail.com>
2023-06-25 11:43:52 +0300

    add_store_content

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    selectedItem: -1
   },
   getters: {
   },
   mutations: {
+    SELECT (state, {
+      index
+    }) {
+      state.selectedItem = index
+    },
+    RESET_SELECTION (state) {
+      state.selectedItem = -1
+    }
   },
   actions: {
   },
