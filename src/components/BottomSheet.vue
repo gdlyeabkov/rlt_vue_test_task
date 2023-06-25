472eb6777f655d411de5d27ca65dd160b9d80582
internetlead <glebdyakov2000@gmail.com>
2023-06-25 11:54:03 +0300

    add_styles_for_bottom_sheet

--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -7,5 +7,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.bottomSheet {
+    height: 72px;
+    background-color: transparent;
+    border: 1px solid #4D4D4D;
+    border-radius: 12px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 25px;
+    &__plate {
+        height: 36px;
+        background-color: #3C3C3C;
+        border-radius: 12px;
+        margin: 18px;
+        flex-grow: 0.9;
+    }
+    &__close {
+        width: 12px;
+        height: 12px;
+        background-image: url('../assets/close.svg');
+        margin: 14px;
+        cursor: pointer;
+    }
+}
 </style>
